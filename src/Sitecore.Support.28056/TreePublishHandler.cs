57eda3f22a6779732cb2075ab624620aea00d2e3
DK\alu <alu@sitecore.net>
2017-10-13 11:36:36 +0300

    Formatting the code

--- src/Sitecore.Support.28056/TreePublishHandler.cs
+++ src/Sitecore.Support.28056/TreePublishHandler.cs
@@ -18,158 +18,158 @@ using Sitecore.Framework.Publishing.PublishJobQueue;
 
 namespace Sitecore.Support.Framework.Publishing.PublishJobQueue.Handlers
 {
-    public class TreePublishHandler : BaseHandler
+  public class TreePublishHandler : BaseHandler
+  {
+    public TreePublishHandler(
+        IRequiredPublishFieldsResolver requiredPublishFieldsResolver,
+        IPublisherOperationService publisherOpsService,
+        IPromotionCoordinator promoterCoordinator,
+        IEventRegistry eventRegistry,
+        ILoggerFactory loggerFactory,
+        IApplicationLifetime applicationLifetime,
+        PublishJobHandlerOptions options = null) : base(
+            requiredPublishFieldsResolver,
+            publisherOpsService,
+            promoterCoordinator,
+            eventRegistry,
+            loggerFactory,
+            applicationLifetime,
+            options ?? new PublishJobHandlerOptions())
     {
-        public TreePublishHandler(
-            IRequiredPublishFieldsResolver requiredPublishFieldsResolver,
-            IPublisherOperationService publisherOpsService,
-            IPromotionCoordinator promoterCoordinator,
-            IEventRegistry eventRegistry,
-            ILoggerFactory loggerFactory,
-            IApplicationLifetime applicationLifetime,
-            PublishJobHandlerOptions options = null) : base(
-                requiredPublishFieldsResolver,
-                publisherOpsService,
-                promoterCoordinator,
-                eventRegistry,
-                loggerFactory,
-                applicationLifetime,
-                options ?? new PublishJobHandlerOptions())
-        {
 
-        }
-
-        public TreePublishHandler(
-            IRequiredPublishFieldsResolver requiredPublishFieldsResolver,
-            IPublisherOperationService publisherOpsService,
-            IPromotionCoordinator promoterCoordinator,
-            IEventRegistry eventRegistry,
-            ILoggerFactory loggerFactory,
-            IApplicationLifetime applicationLifetime,
-            IConfiguration config) : this(
-                requiredPublishFieldsResolver,
-                publisherOpsService,
-                promoterCoordinator,
-                eventRegistry,
-                loggerFactory,
-                applicationLifetime,
-                config.As<PublishJobHandlerOptions>())
-        { }
-
-        #region Factories
-
-        protected override ISourceObservable<CandidateValidationContext> CreatePublishSourceStream(
-            PublishContext publishContext,
-            IPublishCandidateSource publishSourceRepository,
-            IPublishValidator validator,
-            IPublisherOperationService publisherOperationService,
-            CancellationTokenSource errorSource)
-        {
-            var startNode = publishSourceRepository.GetNode(publishContext.PublishOptions.ItemId.Value).Result;
-
-            if (startNode == null)
-                throw new ArgumentNullException($"The publish could not be performed from a start item that doesn't exist : {publishContext.PublishOptions.ItemId.Value}.");
-
-            var parentNode = startNode.ParentId != null ?
-                publishSourceRepository.GetNode(startNode.ParentId.Value).Result :
-                startNode;
-
-            ISourceObservable<CandidateValidationContext> publishSourceStream = new TreeNodeSourceProducer(
-                publishSourceRepository,
-                startNode,
-                validator,
-                publishContext.PublishOptions.Descendants,
-                _options.SourceTreeReaderBatchSize,
-                errorSource,
-                _loggerFactory.CreateLogger<TreeNodeSourceProducer>(),
-                _loggerFactory.CreateLogger<DiagnosticLogger>());
-
-            if (publishContext.PublishOptions.GetItemBucketsEnabled() && parentNode.Node.Properties.TemplateId == publishContext.PublishOptions.GetBucketTemplateId())
-            {
-                publishSourceStream = new BucketNodeSourceProducer(
-                    publishSourceStream,
-                    publishSourceRepository,
-                    startNode,
-                    publishContext.PublishOptions.GetBucketTemplateId(),
-                    errorSource,
-                    _loggerFactory.CreateLogger<BucketNodeSourceProducer>(),
-                    _loggerFactory.CreateLogger<DiagnosticLogger>());
-            }
-
-            return publishSourceStream;
-        }
-
-        protected override IObservable<CandidateValidationTargetContext> CreateTargetProcessingStream(
-            PublishContext publishContext,
-            IPublishCandidateSource publishSourceRepository,
-            IPublishValidator validator,
-            IObservable<CandidateValidationContext> publishStream,
-            ITargetItemIndexService targetIndex,
-            IRequiredPublishFieldsResolver requiredPublishFieldsResolver,
-            CancellationTokenSource errorSource,
-            Guid targetId)
-        {
-            //   Source items -Create target publish stream->PublishCandidateTargetContext
-            IPublishCandidateTargetValidator parentValidator = null;
-            if (publishContext.PublishOptions.GetItemBucketsEnabled())
-            {
-                parentValidator = new PublishTargetBucketParentValidator(publishSourceRepository, targetIndex, publishContext.PublishOptions.GetBucketTemplateId());
-            }
-            else
-            {
-                parentValidator = new PublishTargetParentValidator(publishSourceRepository, targetIndex);
-            }
-
-            publishStream = new CandidatesParentValidationTargetProducer(
-                publishStream,
-                parentValidator,
-                errorSource,
-                publishContext.SourceStore.GetItemReadRepository(),
-                _loggerFactory.CreateLogger<CandidatesParentValidationTargetProducer>(),
-                _loggerFactory.CreateLogger<DiagnosticLogger>());
-
-            publishStream = new CandidatesValidationTargetProducer(
-                publishStream,
-                validator,
-                targetId,
-                errorSource,
-                _loggerFactory.CreateLogger<CandidatesValidationTargetProducer>(),
-                 _loggerFactory.CreateLogger<DiagnosticLogger>());
-
-            if (_options.DeleteOphanedItems && publishContext.PublishOptions.Descendants)
-            {
-                var orphanStream = new OrphanedItemValidationTargetProducer(publishStream,
-                    targetIndex,
-                    publishContext.SourceStore.GetItemReadRepository(),
-                    _options,
-                    errorSource,
-                    _loggerFactory.CreateLogger<OrphanedItemValidationTargetProducer>(),
-                     _loggerFactory.CreateLogger<DiagnosticLogger>());
-
-                publishStream = publishStream.Merge(orphanStream);
-            }
-
-            return base.CreateTargetProcessingStream(
-                publishContext,
-                publishSourceRepository,
-                validator,
-                publishStream,
-                targetIndex,
-                requiredPublishFieldsResolver,
-                errorSource,
-                targetId);
-        }
-
-        #endregion
-
-        public override bool CanHandle(PublishJob job, PublishContext publishContext) => job.Options.ItemId.HasValue;
-
-        protected override async Task UpdateTargetSyncState(PublishContext context, IEnumerable<IManifestOperationResult> promotionResults)
-        {
-            if (context.PublishOptions.ItemId == PublishingConstants.ItemTreeRootId && context.PublishOptions.Descendants)
-            {
-                await base.UpdateTargetSyncState(context, promotionResults).ConfigureAwait(false);
-            }
-        }
     }
+
+    public TreePublishHandler(
+        IRequiredPublishFieldsResolver requiredPublishFieldsResolver,
+        IPublisherOperationService publisherOpsService,
+        IPromotionCoordinator promoterCoordinator,
+        IEventRegistry eventRegistry,
+        ILoggerFactory loggerFactory,
+        IApplicationLifetime applicationLifetime,
+        IConfiguration config) : this(
+            requiredPublishFieldsResolver,
+            publisherOpsService,
+            promoterCoordinator,
+            eventRegistry,
+            loggerFactory,
+            applicationLifetime,
+            config.As<PublishJobHandlerOptions>())
+    { }
+
+    #region Factories
+
+    protected override ISourceObservable<CandidateValidationContext> CreatePublishSourceStream(
+        PublishContext publishContext,
+        IPublishCandidateSource publishSourceRepository,
+        IPublishValidator validator,
+        IPublisherOperationService publisherOperationService,
+        CancellationTokenSource errorSource)
+    {
+      var startNode = publishSourceRepository.GetNode(publishContext.PublishOptions.ItemId.Value).Result;
+
+      if (startNode == null)
+        throw new ArgumentNullException($"The publish could not be performed from a start item that doesn't exist : {publishContext.PublishOptions.ItemId.Value}.");
+
+      var parentNode = startNode.ParentId != null ?
+          publishSourceRepository.GetNode(startNode.ParentId.Value).Result :
+          startNode;
+
+      ISourceObservable<CandidateValidationContext> publishSourceStream = new TreeNodeSourceProducer(
+          publishSourceRepository,
+          startNode,
+          validator,
+          publishContext.PublishOptions.Descendants,
+          _options.SourceTreeReaderBatchSize,
+          errorSource,
+          _loggerFactory.CreateLogger<TreeNodeSourceProducer>(),
+          _loggerFactory.CreateLogger<DiagnosticLogger>());
+
+      if (publishContext.PublishOptions.GetItemBucketsEnabled() && parentNode.Node.Properties.TemplateId == publishContext.PublishOptions.GetBucketTemplateId())
+      {
+        publishSourceStream = new BucketNodeSourceProducer(
+            publishSourceStream,
+            publishSourceRepository,
+            startNode,
+            publishContext.PublishOptions.GetBucketTemplateId(),
+            errorSource,
+            _loggerFactory.CreateLogger<BucketNodeSourceProducer>(),
+            _loggerFactory.CreateLogger<DiagnosticLogger>());
+      }
+
+      return publishSourceStream;
+    }
+
+    protected override IObservable<CandidateValidationTargetContext> CreateTargetProcessingStream(
+        PublishContext publishContext,
+        IPublishCandidateSource publishSourceRepository,
+        IPublishValidator validator,
+        IObservable<CandidateValidationContext> publishStream,
+        ITargetItemIndexService targetIndex,
+        IRequiredPublishFieldsResolver requiredPublishFieldsResolver,
+        CancellationTokenSource errorSource,
+        Guid targetId)
+    {
+      //   Source items -Create target publish stream->PublishCandidateTargetContext
+      IPublishCandidateTargetValidator parentValidator = null;
+      if (publishContext.PublishOptions.GetItemBucketsEnabled())
+      {
+        parentValidator = new PublishTargetBucketParentValidator(publishSourceRepository, targetIndex, publishContext.PublishOptions.GetBucketTemplateId());
+      }
+      else
+      {
+        parentValidator = new PublishTargetParentValidator(publishSourceRepository, targetIndex);
+      }
+
+      publishStream = new CandidatesParentValidationTargetProducer(
+          publishStream,
+          parentValidator,
+          errorSource,
+          publishContext.SourceStore.GetItemReadRepository(),
+          _loggerFactory.CreateLogger<CandidatesParentValidationTargetProducer>(),
+          _loggerFactory.CreateLogger<DiagnosticLogger>());
+
+      publishStream = new CandidatesValidationTargetProducer(
+          publishStream,
+          validator,
+          targetId,
+          errorSource,
+          _loggerFactory.CreateLogger<CandidatesValidationTargetProducer>(),
+           _loggerFactory.CreateLogger<DiagnosticLogger>());
+
+      if (_options.DeleteOphanedItems && publishContext.PublishOptions.Descendants)
+      {
+        var orphanStream = new OrphanedItemValidationTargetProducer(publishStream,
+            targetIndex,
+            publishContext.SourceStore.GetItemReadRepository(),
+            _options,
+            errorSource,
+            _loggerFactory.CreateLogger<OrphanedItemValidationTargetProducer>(),
+             _loggerFactory.CreateLogger<DiagnosticLogger>());
+
+        publishStream = publishStream.Merge(orphanStream);
+      }
+
+      return base.CreateTargetProcessingStream(
+          publishContext,
+          publishSourceRepository,
+          validator,
+          publishStream,
+          targetIndex,
+          requiredPublishFieldsResolver,
+          errorSource,
+          targetId);
+    }
+
+    #endregion
+
+    public override bool CanHandle(PublishJob job, PublishContext publishContext) => job.Options.ItemId.HasValue;
+
+    protected override async Task UpdateTargetSyncState(PublishContext context, IEnumerable<IManifestOperationResult> promotionResults)
+    {
+      if (context.PublishOptions.ItemId == PublishingConstants.ItemTreeRootId && context.PublishOptions.Descendants)
+      {
+        await base.UpdateTargetSyncState(context, promotionResults).ConfigureAwait(false);
+      }
+    }
+  }
 }
